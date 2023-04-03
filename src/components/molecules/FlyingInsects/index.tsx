1d53404ee80170776d22b4f0c5643ed7f349eb47
Mike <michele00.pulvirenti@gmail.com>
2023-04-03 11:42:26 +0200

    Resolved bugs

--- src/components/molecules/FlyingInsects/index.tsx
+++ src/components/molecules/FlyingInsects/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import FlyingInsect from '../../atoms/FlyingInsect';
+import * as styles from './index.module.scss';
 
 const Index = () => {
   const [catched, setCatched] = useState<number>(0);
@@ -13,12 +14,12 @@ const Index = () => {
   }, [catched]);
 
   return (
-    <>
+    <span className={styles.wrap}>
       <FlyingInsect top={30} onClick={handleClick} />
       <FlyingInsect top={50} onClick={handleClick} />
       <FlyingInsect top={70} onClick={handleClick} />
       <FlyingInsect top={20} onClick={handleClick} />
-    </>
+    </span>
   );
 };
 
