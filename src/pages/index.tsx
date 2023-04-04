c33203ccacf930b419a7ea809c1f858d1accc164
Mike <michele00.pulvirenti@gmail.com>
2023-04-04 13:16:57 +0200

    Adding seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,15 @@ import type { PageProps } from 'gatsby';
 import Layout from '../components/organisms/Layout';
 import HeroHome from '../components/organisms/HeroHome';
 import './../styles/globals.scss';
+import Head from '../components/atoms/Seo/Head';
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout>
+      <Head
+        title="Home"
+        description="L'evento si propone di essere occasione di ritrovo della community di Syskrack nonché strumento di disseminazione culturale sul territorio."
+      />
       <HeroHome />
     </Layout>
   );
