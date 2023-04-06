15d8d3c2d45cf87f1b4acbfa81100559150cae1c
Mike <michele00.pulvirenti@gmail.com>
2023-04-06 21:23:55 +0200

    Edited 404 page and added chi-siamo page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Seo from '../components/atoms/Seo';
+import Layout from '../components/organisms/Layout';
+import FlyingInsects from '../components/molecules/FlyingInsects';
+import * as styles from '../styles/NotFound.module.scss';
 
 const NotFoundPage = () => {
   return (
-    <main>
-      <h1>Page not found</h1>
-      <p>
-        Sorry 😔, we couldn't find what you were looking for.
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <Layout>
+      <FlyingInsects />
+      <div className={styles.wrap}>
+        <h1>
+          404
+          <br />
+          Syskrack not found
+        </h1>
+        <p>We couldn't find what you were looking for.</p>
+        <Link className={styles.button} to="/">
+          Go home
+        </Link>
+      </div>
+    </Layout>
   );
 };
+
+export const Head = () => <Seo title="404" description="Error 404, Syskrack not found" />;
 
 export default NotFoundPage;
---
+++ src/pages/chi-siamo.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Layout from '../components/organisms/Layout';
+
+const ChiSiamo = () => {
+  return <Layout>Syskrack</Layout>;
+};
+
+export default ChiSiamo;
