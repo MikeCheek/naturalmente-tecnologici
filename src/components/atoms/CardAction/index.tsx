5ec0d3f1b62bbe15a945798ebbd9ea8f6fbfc22e
Mike <michele00.pulvirenti@gmail.com>
2023-04-28 01:11:10 +0200

    Added animation

--- src/components/atoms/CardAction/index.tsx
+++ src/components/atoms/CardAction/index.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { FastActionProps } from './index.types';
 import { Link } from 'gatsby';
 import * as styles from './index.module.scss';
+import { useInView } from 'react-intersection-observer';
 
 const Index = ({ text, icon, buttonHref, buttonText, linkType = 'external' }: FastActionProps) => {
+  const [ref, inView, _entry] = useInView({
+    threshold: 0,
+    rootMargin: '5% 0px -20% 0px',
+    fallbackInView: true,
+    // triggerOnce: true,
+  });
+
   return (
-    <div className={styles.wrap}>
+    <div className={inView ? styles.wrap : styles.wrapHidden} ref={ref}>
       {icon}
       <p dangerouslySetInnerHTML={{ __html: text }}></p>
       {linkType == 'external' ? (
