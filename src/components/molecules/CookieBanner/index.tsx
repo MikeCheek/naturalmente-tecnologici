c72ca1adeec2c263a321047b1cb92c356a92a8bd
Mike <michele00.pulvirenti@gmail.com>
2023-04-30 23:50:07 +0200

    Fixing and finishing cookie banner

--- src/components/molecules/CookieBanner/index.tsx
+++ src/components/molecules/CookieBanner/index.tsx
@@ -3,6 +3,7 @@ import CookieConsent, { Cookies, getCookieConsentValue } from 'react-cookie-cons
 import * as styles from './index.module.scss';
 import X from '../../../assets/x.svg';
 import CookieBannerProps from './index.types';
+import Down from '../../../assets/down.svg';
 
 const expires = 150;
 const cookieConsentName = 'cmplz_consented_services';
@@ -13,6 +14,7 @@ const Index = ({ close }: CookieBannerProps) => {
   const [showOptions, setShowOptions] = useState<boolean>(false);
   const [marketing, setMarketing] = useState<string>('allow');
   const [statistics, setStatistics] = useState<string>('allow');
+  const [info, setInfo] = useState<number>();
 
   const baseSettings = () => {
     Cookies.set('cmplz_banner-status', 'dismissed', options);
@@ -47,6 +49,8 @@ const Index = ({ close }: CookieBannerProps) => {
     setShowOptions(true);
   };
 
+  const handleDownClick = (id: number) => setInfo((curr) => (curr === id ? undefined : id));
+
   const isAllowed = (value: string) => (value === 'allow' ? true : false);
   const toggleMarketing = () => setMarketing((curr) => (curr === 'allow' ? 'deny' : 'allow'));
   const toggleStatistics = () => setStatistics((curr) => (curr === 'allow' ? 'deny' : 'allow'));
@@ -73,7 +77,7 @@ const Index = ({ close }: CookieBannerProps) => {
       per inviarti pubblicità in linea con le tue preferenze; per accettare l'utilizzo di questi cookie clicca su
       “Accetta tutti”, mentre cliccando sul tasto “X” precludi la loro installazione; se, invece, vuoi operare una
       scelta analitica su specifici cookie{' '}
-      <span className={styles.link} onClick={openOptions}>
+      <span className={styles.link} style={{ display: 'contents' }} onClick={openOptions}>
         personalizza le tue preferenze
       </span>
       .
@@ -84,22 +88,74 @@ const Index = ({ close }: CookieBannerProps) => {
             <div className={styles.functional}>
               <p>Sempre attivo</p>
               <span className={styles.checkboxChecked}></span>
+              <Down
+                className={styles.down}
+                style={info === 0 ? { transform: 'rotate(180deg)' } : {}}
+                width={20}
+                height={20}
+                onClick={() => handleDownClick(0)}
+              />
             </div>
           </div>
+          {info === 0 ? (
+            <p className={styles.info}>
+              L'archiviazione tecnica o l'accesso sono strettamente necessari al fine legittimo di consentire l'uso di
+              un servizio specifico esplicitamente richiesto dall'abbonato o dall'utente, o al solo scopo di effettuare
+              la trasmissione di una comunicazione su una rete di comunicazione elettronica.
+            </p>
+          ) : (
+            <></>
+          )}
           <div className={styles.option}>
             <p>Statistiche</p>
-            <span
-              className={isAllowed(statistics) ? styles.checkboxChecked : styles.checkbox}
-              onClick={toggleStatistics}
-            ></span>
+            <div className={styles.actions}>
+              <span
+                className={isAllowed(statistics) ? styles.checkboxChecked : styles.checkbox}
+                onClick={toggleStatistics}
+              ></span>
+              <Down
+                className={styles.down}
+                style={info === 1 ? { transform: 'rotate(180deg)' } : {}}
+                width={20}
+                height={20}
+                onClick={() => handleDownClick(1)}
+              />
+            </div>
           </div>
+          {info === 1 ? (
+            <p className={styles.info}>
+              L'archiviazione tecnica o l'accesso che viene utilizzato esclusivamente per scopi statistici anonimi.
+              Senza un mandato di comparizione, una conformità volontaria da parte del vostro Fornitore di Servizi
+              Internet, o ulteriori registrazioni da parte di terzi, le informazioni memorizzate o recuperate per questo
+              scopo da sole non possono di solito essere utilizzate per l'identificazione.
+            </p>
+          ) : (
+            <></>
+          )}
           <div className={styles.option}>
             <p>Marketing</p>
-            <span
-              className={isAllowed(marketing) ? styles.checkboxChecked : styles.checkbox}
-              onClick={toggleMarketing}
-            ></span>
+            <div className={styles.actions}>
+              <span
+                className={isAllowed(marketing) ? styles.checkboxChecked : styles.checkbox}
+                onClick={toggleMarketing}
+              ></span>
+              <Down
+                className={styles.down}
+                style={info === 2 ? { transform: 'rotate(180deg)' } : {}}
+                width={20}
+                height={20}
+                onClick={() => handleDownClick(2)}
+              />
+            </div>
           </div>
+          {info === 2 ? (
+            <p className={styles.info}>
+              L'archiviazione tecnica o l'accesso sono necessari per creare profili di utenti per inviare pubblicità, o
+              per tracciare l'utente su un sito web o su diversi siti web per scopi di marketing simili.
+            </p>
+          ) : (
+            <></>
+          )}
         </div>
       ) : (
         <></>
