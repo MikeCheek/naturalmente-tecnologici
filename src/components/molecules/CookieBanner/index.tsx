be3f1e51c055b5826a8c835dc063274f847ec51b
Mike <michele00.pulvirenti@gmail.com>
2023-05-05 18:52:27 +0200

    Removed useless plugin and fixed cookie banner

--- src/components/molecules/CookieBanner/index.tsx
+++ src/components/molecules/CookieBanner/index.tsx
@@ -1,29 +1,20 @@
-import React, { useState } from 'react';
-import CookieConsent, { Cookies } from 'react-cookie-consent';
+import React, { useEffect, useState } from 'react';
 import * as styles from './index.module.scss';
 import X from '../../../assets/x.svg';
 import CookieBannerProps from './index.types';
 import Down from '../../../assets/down.svg';
-
-const expires = 150;
-
-const CookiesNames = {
-  functional: 'cmplz_functional',
-  statistics: 'cmplz_statistics',
-  marketing: 'cmplz_marketing',
-};
+import Cookies from 'js-cookie';
+import { CookiesNames, options } from '../../../hooks/cookies';
 
 const Index = ({ close }: CookieBannerProps) => {
-  const options = { expires: expires, secure: true, sameSite: 'Lax' };
-
   const [showOptions, setShowOptions] = useState<boolean>(false);
   const [marketing, setMarketing] = useState<string>('allow');
   const [statistics, setStatistics] = useState<string>('allow');
   const [info, setInfo] = useState<number>();
 
-  const handleAcceptCookie = (close?: () => void) => {
+  const handleAcceptCookie = (denyAll: boolean = false) => {
     Cookies.set(CookiesNames.functional, 'allow', options);
-    if (close) {
+    if (denyAll) {
       Cookies.set(CookiesNames.statistics, 'deny', options);
       Cookies.set(CookiesNames.marketing, 'deny', options);
     } else {
@@ -32,10 +23,7 @@ const Index = ({ close }: CookieBannerProps) => {
     }
     //@ts-ignore
     window.dataLayer?.push({ event: 'update-consent' });
-
-    if (close) {
-      close();
-    }
+    close();
   };
 
   const openOptions = () => {
@@ -51,119 +39,123 @@ const Index = ({ close }: CookieBannerProps) => {
   const toggleStatistics = () => setStatistics((curr) => (curr === 'allow' ? 'deny' : 'allow'));
 
   return (
-    <CookieConsent
-      onAccept={() => handleAcceptCookie()}
-      location="bottom"
-      buttonText={showOptions ? 'Salva preferenze' : 'Accetta tutti'}
-      cookieName={CookiesNames.functional}
-      expires={expires}
-      disableStyles
-      cookieSecurity={true}
-      buttonWrapperClasses={styles.buttons}
-      buttonClasses={styles.acceptButton}
-      containerClasses={styles.banner}
-      contentClasses={styles.content}
-      overlay
-    >
-      <X className={styles.close} width={20} height={20} onClick={() => handleAcceptCookie(close)} />
-      Questo sito utilizza cookie tecnici per il proprio funzionamento e per l'erogazione dei relativi servizi; per tali
-      cookie non è richiesto il tuo consenso. Potrebbero, inoltre, essere installati cookie analitici (propri e di terza
-      parte) per comprendere meglio il tuo utilizzo di questo sito e cookie di profilazione (propri e di terze parti)
-      per inviarti pubblicità in linea con le tue preferenze; per accettare l'utilizzo di questi cookie clicca su
-      “Accetta tutti”, mentre cliccando sul tasto “X” precludi la loro installazione; se, invece, vuoi operare una
-      scelta analitica su specifici cookie{' '}
-      <span className={styles.link} style={{ display: 'contents' }} onClick={openOptions}>
-        personalizza le tue preferenze
-      </span>
-      .
-      {showOptions ? (
-        <div className={styles.options}>
-          <div className={styles.option}>
-            <p>Funzionale</p>
-            <div className={styles.functional}>
-              <p>Sempre attivo</p>
-              <span className={styles.checkboxChecked}></span>
-              <Down
-                className={styles.down}
-                style={info === 0 ? { transform: 'rotate(180deg)' } : {}}
-                width={20}
-                height={20}
-                onClick={() => handleDownClick(0)}
-              />
+    <>
+      <div className={styles.shadow}></div>
+      <div className={styles.banner}>
+        <X className={styles.close} width={20} height={20} onClick={() => handleAcceptCookie(true)} />
+        <div className={styles.content}>
+          Questo sito utilizza cookie tecnici per il proprio funzionamento e per l'erogazione dei relativi servizi; per
+          tali cookie non è richiesto il tuo consenso. Potrebbero, inoltre, essere installati cookie analitici (propri e
+          di terza parte) per comprendere meglio il tuo utilizzo di questo sito e cookie di profilazione (propri e di
+          terze parti) per inviarti pubblicità in linea con le tue preferenze; per accettare l'utilizzo di questi cookie
+          clicca su “Accetta tutti”, mentre cliccando sul tasto “X” precludi la loro installazione; se, invece, vuoi
+          operare una scelta analitica su specifici cookie{' '}
+          <span className={styles.link} style={{ display: 'contents' }} onClick={openOptions}>
+            personalizza le tue preferenze
+          </span>
+          .
+          {showOptions ? (
+            <div className={styles.options}>
+              <div className={styles.option}>
+                <p>Funzionale</p>
+                <div className={styles.functional}>
+                  <p>Sempre attivo</p>
+                  <span className={styles.checkboxChecked}></span>
+                  <Down
+                    className={styles.down}
+                    style={info === 0 ? { transform: 'rotate(180deg)' } : {}}
+                    width={20}
+                    height={20}
+                    onClick={() => handleDownClick(0)}
+                  />
+                </div>
+              </div>
+              {info === 0 ? (
+                <p className={styles.info}>
+                  L'archiviazione tecnica o l'accesso sono strettamente necessari al fine legittimo di consentire l'uso
+                  di un servizio specifico esplicitamente richiesto dall'abbonato o dall'utente, o al solo scopo di
+                  effettuare la trasmissione di una comunicazione su una rete di comunicazione elettronica.
+                </p>
+              ) : (
+                <></>
+              )}
+              <div className={styles.option}>
+                <p>Statistiche</p>
+                <div className={styles.actions}>
+                  <span
+                    className={isAllowed(statistics) ? styles.checkboxChecked : styles.checkbox}
+                    onClick={toggleStatistics}
+                  ></span>
+                  <Down
+                    className={styles.down}
+                    style={info === 1 ? { transform: 'rotate(180deg)' } : {}}
+                    width={20}
+                    height={20}
+                    onClick={() => handleDownClick(1)}
+                  />
+                </div>
+              </div>
+              {info === 1 ? (
+                <p className={styles.info}>
+                  L'archiviazione tecnica o l'accesso che viene utilizzato esclusivamente per scopi statistici anonimi.
+                  Senza un mandato di comparizione, una conformità volontaria da parte del vostro Fornitore di Servizi
+                  Internet, o ulteriori registrazioni da parte di terzi, le informazioni memorizzate o recuperate per
+                  questo scopo da sole non possono di solito essere utilizzate per l'identificazione.
+                </p>
+              ) : (
+                <></>
+              )}
+              <div className={styles.option}>
+                <p>Marketing</p>
+                <div className={styles.actions}>
+                  <span
+                    className={isAllowed(marketing) ? styles.checkboxChecked : styles.checkbox}
+                    onClick={toggleMarketing}
+                  ></span>
+                  <Down
+                    className={styles.down}
+                    style={info === 2 ? { transform: 'rotate(180deg)' } : {}}
+                    width={20}
+                    height={20}
+                    onClick={() => handleDownClick(2)}
+                  />
+                </div>
+              </div>
+              {info === 2 ? (
+                <p className={styles.info}>
+                  L'archiviazione tecnica o l'accesso sono necessari per creare profili di utenti per inviare
+                  pubblicità, o per tracciare l'utente su un sito web o su diversi siti web per scopi di marketing
+                  simili.
+                </p>
+              ) : (
+                <></>
+              )}
             </div>
-          </div>
-          {info === 0 ? (
-            <p className={styles.info}>
-              L'archiviazione tecnica o l'accesso sono strettamente necessari al fine legittimo di consentire l'uso di
-              un servizio specifico esplicitamente richiesto dall'abbonato o dall'utente, o al solo scopo di effettuare
-              la trasmissione di una comunicazione su una rete di comunicazione elettronica.
-            </p>
-          ) : (
-            <></>
-          )}
-          <div className={styles.option}>
-            <p>Statistiche</p>
-            <div className={styles.actions}>
-              <span
-                className={isAllowed(statistics) ? styles.checkboxChecked : styles.checkbox}
-                onClick={toggleStatistics}
-              ></span>
-              <Down
-                className={styles.down}
-                style={info === 1 ? { transform: 'rotate(180deg)' } : {}}
-                width={20}
-                height={20}
-                onClick={() => handleDownClick(1)}
-              />
-            </div>
-          </div>
-          {info === 1 ? (
-            <p className={styles.info}>
-              L'archiviazione tecnica o l'accesso che viene utilizzato esclusivamente per scopi statistici anonimi.
-              Senza un mandato di comparizione, una conformità volontaria da parte del vostro Fornitore di Servizi
-              Internet, o ulteriori registrazioni da parte di terzi, le informazioni memorizzate o recuperate per questo
-              scopo da sole non possono di solito essere utilizzate per l'identificazione.
-            </p>
-          ) : (
-            <></>
-          )}
-          <div className={styles.option}>
-            <p>Marketing</p>
-            <div className={styles.actions}>
-              <span
-                className={isAllowed(marketing) ? styles.checkboxChecked : styles.checkbox}
-                onClick={toggleMarketing}
-              ></span>
-              <Down
-                className={styles.down}
-                style={info === 2 ? { transform: 'rotate(180deg)' } : {}}
-                width={20}
-                height={20}
-                onClick={() => handleDownClick(2)}
-              />
-            </div>
-          </div>
-          {info === 2 ? (
-            <p className={styles.info}>
-              L'archiviazione tecnica o l'accesso sono necessari per creare profili di utenti per inviare pubblicità, o
-              per tracciare l'utente su un sito web o su diversi siti web per scopi di marketing simili.
-            </p>
           ) : (
             <></>
           )}
         </div>
-      ) : (
-        <></>
-      )}
-      <div className={styles.links}>
-        <a className={styles.link} target="_blank" title="Cookie Policy" href="https://syskrack.org/cookie-policy-ue/">
-          Cookie Policy
-        </a>
-        <a className={styles.link} target="_blank" title="Privacy Policy" href="https://syskrack.org/privacy-policy/">
-          Privacy Policy
-        </a>
+
+        <div className={styles.buttons}>
+          <button className={styles.acceptButton} onClick={() => handleAcceptCookie()}>
+            {showOptions ? 'Salva preferenze' : 'Accetta tutti'}
+          </button>
+        </div>
+        <div className={styles.links}>
+          <a
+            className={styles.link}
+            target="_blank"
+            title="Cookie Policy"
+            href="https://syskrack.org/cookie-policy-ue/"
+          >
+            Cookie Policy
+          </a>
+          <a className={styles.link} target="_blank" title="Privacy Policy" href="https://syskrack.org/privacy-policy/">
+            Privacy Policy
+          </a>
+        </div>
       </div>
-    </CookieConsent>
+    </>
   );
 };
 
--- src/components/organisms/Layout/index.tsx
+++ src/components/organisms/Layout/index.tsx
@@ -1,13 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from '../NavBar';
 import { LayoutProps } from './index.types';
 import * as styles from './index.module.scss';
 import FlyingInsects from '../../molecules/FlyingInsects';
 import Footer from '../../molecules/Footer';
 import CookieBanner from '../../molecules/CookieBanner';
+import Cookies from 'js-cookie';
+import { CookiesNames } from '../../../hooks/cookies';
 
 const Index = ({ children, insects = true }: LayoutProps) => {
-  const [banner, setBanner] = useState<boolean>(true);
+  const [banner, setBanner] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (Cookies.get(CookiesNames.functional) == undefined) {
+      setBanner(true);
+    }
+  }, []);
 
   return (
     <div id="top">
