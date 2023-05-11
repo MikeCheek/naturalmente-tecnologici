a2f6d03efd9c13eba2b72ab32a8fa9d3a4fef00f
Mike <michele00.pulvirenti@gmail.com>
2023-05-11 15:40:17 +0200

    Edited BreadcrumbList in Structured Data

--- src/hooks/navigation.ts
+++ src/hooks/navigation.ts
@@ -1,7 +1,7 @@
 export const links = [
-  { name: 'Home', to: '/', position: 0 },
-  { name: 'Programma', to: '/programma', position: 1 },
-  { name: 'Contattaci', to: '/contattaci', position: 1 },
-  { name: 'Partner', to: '/partner', position: 1 },
-  { name: 'Chi siamo', to: '/chi-siamo', position: 1 },
+  { name: 'Home', to: '/', position: 1 },
+  { name: 'Programma', to: '/programma', position: 2 },
+  { name: 'Contattaci', to: '/contattaci', position: 2 },
+  { name: 'Partner', to: '/partner', position: 2 },
+  { name: 'Chi siamo', to: '/chi-siamo', position: 2 },
 ];
