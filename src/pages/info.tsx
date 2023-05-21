8d0693e9ca97d699e5262a7967de057b3d3cd68a
Mike <michele00.pulvirenti@gmail.com>
2023-05-21 17:10:45 +0200

    Fixed info/faq page

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '../components/organisms/Layout';
-import HeroContacts from '../components/organisms/HeroContacts';
+import HeroInfo from '../components/organisms/HeroInfo';
 import Seo from '../components/atoms/Seo';
 import Contacts from '../components/molecules/Contacts';
 import Faq from '../components/organisms/Faq';
@@ -8,9 +8,9 @@ import Faq from '../components/organisms/Faq';
 const Info = () => {
   return (
     <Layout>
-      <HeroContacts />
-      <Contacts />
+      <HeroInfo />
       <Faq />
+      <Contacts />
     </Layout>
   );
 };
@@ -19,7 +19,7 @@ export const Head = () => (
   <Seo
     title="Info"
     pathname="/info/"
-    description="Per qualunque collaborazione, intervista, supporto o informazione puoi contattarci a direttivo@syskrack.org"
+    description="Tutto ciò che devi sapere sul festival e molto altro. Per qualunque collaborazione, intervista, supporto o informazione puoi contattarci a direttivo@syskrack.org"
     structuredData
   />
 );
