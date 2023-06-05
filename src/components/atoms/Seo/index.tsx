4c690eab7f649efbc76ce7607413e6859eaf4061
Bernardo Forcillo <bernardoforcillo@outlook.com>
2023-06-05 15:28:22 +0200

    - refactored tickets (json+ld) - linked with eventbrite schema+ld event

--- src/components/atoms/Seo/index.tsx
+++ src/components/atoms/Seo/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { SeoProps } from './index.types';
 import useSiteMetadata from '../../../hooks/useSiteMetadata';
-//import { tickets } from '../../../hooks/useInfo';
 import { links } from '../../../hooks/navigation';
+import { DefaultTicketProps, tickets } from 'src/hooks/useInfo';
 
 const Index = ({
   lang = 'it',
@@ -64,13 +64,13 @@ const Index = ({
           '@type': 'Place',
           name: 'Podus - Bosco Coste',
           url: 'http://www.podus.it/',
-            address: {
-              '@type': 'PostalAddress',
-              streetAddress: 'Bosco Coste',
-              addressLocality: 'Grottole',
-              addressRegion: 'MT',
-              postalCode: '75010',
-              addressCountry: 'Italy',
+          address: {
+            '@type': 'PostalAddress',
+            streetAddress: 'Bosco Coste',
+            addressLocality: 'Grottole',
+            addressRegion: 'MT',
+            postalCode: '75010',
+            addressCountry: 'Italy',
           },
           sameAs: ['https://goo.gl/maps/bVJXkHquwbvapgWR6', 'https://goo.gl/maps/YLQ778t7ZbGhkpHt9'],
         },
@@ -81,86 +81,36 @@ const Index = ({
           sameAs: ['https://www.wikidata.org/wiki/Q116907424', 'https://syskrack.org/'],
         },
         typicalAgeRange: '18-',
-        availabilityStarts: '2023-06-1T00:00:00.000Z',
-        availabilityEnds: '2023-08-13T23:59:59.999Z',
-        validFrom: '2023-08-10T18:00:00.000Z',
-        validThrough: '2023-08-14T10:59:59.999Z',
-        offers: {
+        offer: {
           '@type': 'AggregateOffer',
-          highPrice: '72.22',
-          lowPrice: '17.22',
-          offerCount: 3,
-          priceCurrency: 'EUR',
-          offer: [
-            {
-              '@type': 'Offer',
-              url: 'https://www.eventbrite.com/e/registrazione-naturalmente-tecnologici-23-ri-prendiamoci-il-futuro-640095231067',
-              name: 'Biglietto Interno - Early Bird',
-              availability: 'https://schema.org/PreOrder',
-              availabilityStarts: '2023-06-1T00:00:00.000Z',
-              availabilityEnds: '2023-06-16T23:59:59.999Z',
-              validFrom: '2023-08-10T18:00:00.000Z',
-              validThrough: '2023-08-14T10:59:59.999Z',
-              description: 'Il festival offre l\'opzione di ingresso illimitato per tutti e tre i giorni dell\'evento, che include una vasta gamma di attività come laboratori, conferenze, workshop, spettacoli e musica. Inoltre, è possibile utilizzare l\'area camping per l\'intera durata del festival, compresi i servizi come postazioni per ricaricare gli smartphone, docce e bagni. L\'accesso all\'area camping può essere ottenuto a partire dalla sera del 10. La quota di partecipazione al festival comprende l\'accesso completo a tutte le attività dell\'evento, inclusi i workshop, le conferenze, lo stage musicale e gli spettacoli. Tuttavia, la quota non copre i costi per cibi, bevande e merchandise, che devono essere acquistati separatamente. Per poter partecipare al festival, è obbligatorio diventare associati tramite il tesseramento, che può essere completato sul sito web https://syskrack.org/associati.',
-              price: '72.22',
-              priceCurrency: 'EUR',
-            },
-            {
-              '@type': 'Offer',
-              url: 'https://www.eventbrite.com/e/registrazione-naturalmente-tecnologici-23-ri-prendiamoci-il-futuro-640095231067',
-              name: 'Biglietto Giornaliero (11 Agosto) - Early Bird',
-              availability: 'https://schema.org/PreOrder',
-              availabilityStarts: '2023-06-1T00:00:00.000Z',
-              availabilityEnds: '2023-06-16T23:59:59.999Z',
-              validFrom: '2023-08-11T00:00:00.000Z',
-              validThrough: '2023-08-11T23:59:59.999Z',
-              description: 'L\'ingresso giornaliero valido per il giorno 11 agosto 2023 offre la possibilità di partecipare alle attività del festival per quella specifica giornata. Ciò include l\'accesso completo all\'evento, compresi i workshop, le attività, lo stage musicale e gli spettacoli. La quota di partecipazione al biglietto giornaliero non copre i costi per cibi, bevande e merchandise, che devono essere acquistati separatamente. Per poter partecipare al festival, è obbligatorio diventare associati tramite il tesseramento, che può essere completato sul sito web https://syskrack.org/associati.',
-              price: '17.22',
-              priceCurrency: 'EUR',
-            },
-            {
-              '@type': 'Offer',
-              url: 'https://www.eventbrite.com/e/registrazione-naturalmente-tecnologici-23-ri-prendiamoci-il-futuro-640095231067',
-              name: 'Biglietto Giornaliero (12 Agosto) - Early Bird',
-              availability: 'https://schema.org/PreOrder',
-              availabilityStarts: '2023-06-1T00:00:00.000Z',
-              availabilityEnds: '2023-06-16T23:59:59.999Z',
-              validFrom: '2023-08-12T00:00:00.000Z',
-              validThrough: '2023-08-12T23:59:59.999Z',
-              description: 'L\'ingresso giornaliero valido per il giorno 11 agosto 2023 offre la possibilità di partecipare alle attività del festival per quella specifica giornata. Ciò include l\'accesso completo all\'evento, compresi i workshop, le attività, lo stage musicale e gli spettacoli. La quota di partecipazione al biglietto giornaliero non copre i costi per cibi, bevande e merchandise, che devono essere acquistati separatamente. Per poter partecipare al festival, è obbligatorio diventare associati tramite il tesseramento, che può essere completato sul sito web https://syskrack.org/associati.',
-              price: '17.22',
-              priceCurrency: 'EUR',
-            },
-            {
-              '@type': 'Offer',
-              url: 'https://www.eventbrite.com/e/registrazione-naturalmente-tecnologici-23-ri-prendiamoci-il-futuro-640095231067',
-              name: 'Biglietto Giornaliero (13 Agosto) - Early Bird',
-              availability: 'https://schema.org/PreOrder',
-              availabilityStarts: '2023-06-1T00:00:00.000Z',
-              availabilityEnds: '2023-06-16T23:59:59.999Z',
-              validFrom: '2023-08-13T00:00:00.000Z',
-              validThrough: '2023-08-13T23:59:59.999Z',
-              description: 'L\'ingresso giornaliero valido per il giorno 11 agosto 2023 offre la possibilità di partecipare alle attività del festival per quella specifica giornata. Ciò include l\'accesso completo all\'evento, compresi i workshop, le attività, lo stage musicale e gli spettacoli. La quota di partecipazione al biglietto giornaliero non copre i costi per cibi, bevande e merchandise, che devono essere acquistati separatamente. Per poter partecipare al festival, è obbligatorio diventare associati tramite il tesseramento, che può essere completato sul sito web https://syskrack.org/associati.',
-              price: '17.22',
-              priceCurrency: 'EUR',
-            }
+          availabilityStarts: '2023-06-1T00:00:00.000Z',
+          availabilityEnds: '2023-08-13T23:59:59.999Z',
+          validFrom: '2023-08-10T18:00:00.000Z',
+          validThrough: '2023-08-14T10:59:59.999Z',
+          highPrice: Math.max(...tickets.map((ticket) => ticket.price)),
+          lowPrice: Math.min(...tickets.map((ticket) => ticket.price)),
+          offerCount: tickets.length,
+          sameAs: [
+            'https://www.eventbrite.com/e/registrazione-naturalmente-tecnologici-23-ri-prendiamoci-il-futuro-640095231067'
           ],
-          //offer: tickets.map((ticket) => ({
-          //  '@type': 'Offer',
-          //  url: ticket.url,
-          //  name: ticket.name,
-          //  availability: 'https://schema.org/PreOrder',
-          //  validFrom: '2023-08-11T09:00:00.000Z',
-          //  validThrough: '2023-08-13T23:59:59.999Z',
-          //  description: ticket.name,
-          //  price: ticket.price,
-          //  priceCurrency: 'EUR',
-          //})),
-         },
+          offers: tickets.map((ticket) => ({
+            '@type': 'Offer',
+            url: DefaultTicketProps.url,
+            name: ticket.name,
+            availability: 'https://schema.org/PreOrder',
+            availabilityStarts: ticket.availabilityStarts,
+            availabilityEnds: ticket.availabilityEnds,
+            validFrom: ticket.validFrom,
+            validThrough: ticket.validThrough,
+            description: ticket.description,
+            price: ticket.price,
+            priceCurrency: DefaultTicketProps.priceCurrency,
+          })),
+        },
         sameAs: [
           'https://www.wikidata.org/wiki/Q117883453', // INFO: (Link evento [2023] aggiunto su Wikidata)
           'https://www.wikidata.org/wiki/Q117881465', // INFO: (Link evento aggiunto su Wikidata)
-          'https://www.eventbrite.com/e/registrazione-naturalmente-tecnologici-23-ri-prendiamoci-il-futuro-640095231067?utm-campaign=social,email&utm-content=attendeeshare&utm-medium=discovery&utm-source=strongmail&utm-term=listing'
+          'https://www.eventbrite.com/e/registrazione-naturalmente-tecnologici-23-ri-prendiamoci-il-futuro-640095231067'
         ],
       },
     ],
