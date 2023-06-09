1cb43ff5f693e6be62b604b214cb721f4807e509
Mike <michele00.pulvirenti@gmail.com>
2023-06-09 15:33:28 +0200

    Added animations

--- src/components/atoms/CardImage/index.tsx
+++ src/components/atoms/CardImage/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CardImageProps } from './index.types';
 import * as styles from './index.module.scss';
 
@@ -9,10 +9,26 @@ const Index = ({
   onlyMobile = false,
   bigger = false,
 }: CardImageProps) => {
+  const [rotate, setRotate] = useState<boolean>(false);
+  const [smaller, setSmaller] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setRotate((curr) => !curr);
+    setSmaller(true);
+    setTimeout(() => setSmaller(false), 500);
+  };
+
   return (
-    <div className={`${styles.wrap} ${onlyMobile ? styles.onlyMobile : ''} ${onlyDesktop ? styles.onlyDesktop : ''}`}>
-      <div className={reversed ? styles.orangeReversed : styles.orange}></div>
-      <div className={bigger ? styles.sectionImage : styles.image}>{children}</div>
+    <div
+      className={`${styles.wrap} ${onlyMobile ? styles.onlyMobile : ''} ${onlyDesktop ? styles.onlyDesktop : ''}`}
+      onClick={handleClick}
+    >
+      <div className={styles.orangeWrap} style={rotate ? { transform: 'rotate(360deg)' } : { transform: 'rotate(0)' }}>
+        <div className={reversed ? styles.orangeReversed : styles.orange}></div>
+      </div>
+      <div className={bigger ? styles.sectionImage : styles.image} style={smaller ? { transform: 'scale(0.9)' } : {}}>
+        {children}
+      </div>
     </div>
   );
 };
