f2d939a2f661d489335e9e50edf186232865a7e3
Mike <michele00.pulvirenti@gmail.com>
2023-06-15 14:43:01 +0200

    Fixing forms

--- src/templates/Collabora/index.tsx
+++ src/templates/Collabora/index.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { CollaboraData } from '../../utilities/collaboraData';
 import * as styles from './index.module.scss';
 import Seo from '../../components/atoms/Seo';
 import { HeadProps } from 'gatsby';
 import Layout from '../../components/organisms/Layout';
-import Loading from '../../components/molecules/Loading';
 import HeroPartecipa from '../../components/organisms/HeroPartecipa';
 import Heading from '../../components/atoms/Heading';
 
@@ -13,8 +12,6 @@ interface IndexProps {
 }
 
 const Index = ({ pageContext }: IndexProps) => {
-  const [loading, setLoading] = useState<boolean>(true);
-
   return (
     <Layout>
       <HeroPartecipa heading={pageContext.name} />
@@ -23,17 +20,13 @@ const Index = ({ pageContext }: IndexProps) => {
       </div>
       <Heading text="Contattaci" />
       <div className={styles.formWrap}>
-        {loading ? <Loading noTitle /> : <></>}
         <iframe
           data-tally-src={`https://tally.so/embed/n9XpoK?type=${pageContext.name}&hideTitle=1&transparentBackground=1&dynamicHeight=1`}
           loading="lazy"
           width="100%"
-          height={loading ? '0' : '420'}
+          height="420"
           title="CONTATTACI"
           className={styles.form}
-          onLoad={() => {
-            setLoading(false);
-          }}
         ></iframe>
       </div>
     </Layout>
