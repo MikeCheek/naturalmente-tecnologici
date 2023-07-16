1adcb6356e3a36b6a23819317b33a6ad34cffb0d
Michele Pulvirenti <62357072+MikeCheek@users.noreply.github.com>
2023-07-16 19:51:48 +0200

    Update tickets.ts

--- src/utilities/tickets.ts
+++ src/utilities/tickets.ts
@@ -32,7 +32,7 @@ export const info: Ticket[] = [
     description:
       "Il festival offre l'opzione di ingresso illimitato per tutti e tre i giorni dell'evento, che include una vasta gamma di attività come laboratori, conferenze, workshop, spettacoli e musica. Inoltre, è possibile utilizzare l'area camping per l'intera durata del festival, compresi i servizi come postazioni per ricaricare gli smartphone, docce e bagni. L'accesso all'area camping può essere ottenuto a partire dalla sera del 10. La quota di partecipazione al festival comprende l'accesso completo a tutte le attività dell'evento, inclusi i workshop, le conferenze, lo stage musicale e gli spettacoli. La quota non copre i pasti e le bevande. Per poter partecipare al festival, è obbligatorio diventare associati tramite il tesseramento, che può essere completato sul sito web https://syskrack.org/associati.",
     availabilityStarts: '2023-06-15T00:00:00.000+02:00',
-    availabilityEnds: '2023-07-20T23:59:59.999+02:00',
+    availabilityEnds: '2023-07-30T23:59:59.999+02:00',
     validFrom: '2023-08-10T18:00:00.000+02:00',
     validThrough: '2023-08-14T10:59:59.999+02:00',
     date: '10-11-12-13 Agosto',
@@ -42,7 +42,7 @@ export const info: Ticket[] = [
     icon: Ticket,
     name: 'Ingresso giornaliero Festival () - Early Bird',
     availabilityStarts: '2023-06-15T00:00:00.000+02:00',
-    availabilityEnds: '2023-07-20T23:59:59.999+02:00',
+    availabilityEnds: '2023-07-30T23:59:59.999+02:00',
     validFrom: '2023-08-10T18:00:00.000+02:00',
     validThrough: '2023-08-14T10:59:59.999+02:00',
     description:
@@ -55,7 +55,7 @@ export const info: Ticket[] = [
   //   icon: Ticket,
   //   name: 'Ingresso giornaliero Festival (11 Agosto) - Early Bird',
   //   availabilityStarts: '2023-06-15T00:00:00.000+02:00',
-  //   availabilityEnds: '2023-07-20T23:59:59.999+02:00',
+  //   availabilityEnds: '2023-07-30T23:59:59.999+02:00',
   //   validFrom: '2023-08-11T00:00:00.000+02:00',
   //   validThrough: '2023-08-11T23:59:59.999+02:00',
   //   description:
@@ -67,7 +67,7 @@ export const info: Ticket[] = [
   //   icon: Ticket,
   //   name: 'Ingresso giornaliero Festival (12 Agosto) - Early Bird',
   //   availabilityStarts: '2023-06-15T00:00:00.000+02:00',
-  //   availabilityEnds: '2023-07-20T23:59:59.999+02:00',
+  //   availabilityEnds: '2023-07-30T23:59:59.999+02:00',
   //   validFrom: '2023-08-12T00:00:00.000+02:00',
   //   validThrough: '2023-08-12T23:59:59.999+02:00',
   //   description:
@@ -79,7 +79,7 @@ export const info: Ticket[] = [
   //   icon: Ticket,
   //   name: 'Ingresso giornaliero Festival (13 Agosto) - Early Bird',
   //   availabilityStarts: '2023-06-15T00:00:00.000+02:00',
-  //   availabilityEnds: '2023-07-20T23:59:59.999+02:00',
+  //   availabilityEnds: '2023-07-30T23:59:59.999+02:00',
   //   validFrom: '2023-08-13T00:00:00.000+02:00',
   //   validThrough: '2023-08-13T23:59:59.999+02:00',
   //   description:
@@ -91,7 +91,7 @@ export const info: Ticket[] = [
     icon: Camping,
     name: 'Integrazione Camping (Solo per ingresso giornaliero)',
     availabilityStarts: '2023-06-15T00:00:00.000+02:00',
-    availabilityEnds: '2023-07-20T23:59:59.999+02:00',
+    availabilityEnds: '2023-07-30T23:59:59.999+02:00',
     validFrom: '2023-08-13T00:00:00.000+02:00',
     validThrough: '2023-08-13T23:59:59.999+02:00',
     description:
