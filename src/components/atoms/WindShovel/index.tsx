fe85810244941872707e8112ea94e486530f3fab
Mike <michele00.pulvirenti@gmail.com>
2024-02-19 18:29:42 +0100

    Reduced shovel svg to one

--- src/components/atoms/WindShovel/index.tsx
+++ src/components/atoms/WindShovel/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { WindShovelProps } from './index.types';
 import WindShovel from '../../../assets/windShovel.svg';
-import WindShovelSlow from '../../../assets/windShovelSlow.svg';
 import * as styles from './index.module.scss';
 
 const Index = ({ color, slower = false }: WindShovelProps) => {
-  const ShovelSVG = slower ? WindShovelSlow : WindShovel;
-  return <ShovelSVG className={styles.shovel} width={80} height={193} fill={color} />;
+  return <WindShovel className={slower ? styles.shovelSlower : styles.shovel} width={80} height={193} fill={color} />;
 };
 
 export default Index;
