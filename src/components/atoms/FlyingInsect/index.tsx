0834ed8686c4943f10ce7946fec3897dda0f7c3e
Mike <michele00.pulvirenti@gmail.com>
2024-02-19 19:12:30 +0100

    Smaller flying insects

--- src/components/atoms/FlyingInsect/index.tsx
+++ src/components/atoms/FlyingInsect/index.tsx
@@ -17,7 +17,7 @@ const Index = ({ top = 0, onClick }: InsectProps) => {
       <div className={styles.svgWrap} style={{ animationDelay: top / 5 + 's' }}>
         <Insect
           onClick={handleClick}
-          width={88}
+          width={50}
           fill={on ? 'var(--nt-orange)' : 'var(--nt-dark-green)'}
           style={{ transform: 'rotate(132.04deg)', animationDelay: top / 4 + 's' }}
           className={on ? '' : styles.svg}
